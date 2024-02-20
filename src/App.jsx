9fca79113c53f5c534b2d76486cd5cddb54d09d7
PRANAV RAIKAR <pranavraikar01@gmail.com>
2024-02-20 11:25:05 +0530

    Summarization done successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Component from "./pages/Component";
+import PdfToSummary from "./pages/PdfToSummary";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Component />} />
+        <Route path="/pdf-to-summary" element={<PdfToSummary />} />
       </Routes>
     </BrowserRouter>
   );
