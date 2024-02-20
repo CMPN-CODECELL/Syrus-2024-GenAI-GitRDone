5a270423b5aac1e4a646a6224b13a14dac4f2528
PRANAV RAIKAR <pranavraikar01@gmail.com>
2024-02-21 00:08:18 +0530

    Added a questionaire

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Component from "./pages/Component";
 import PdfToSummary from "./pages/PdfToSummary";
 import Mindmap from "./pages/Mindmap";
+import Questionaire from "./pages/Questionaire";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Component />} />
         <Route path="/pdf-to-summary" element={<PdfToSummary />} />
         <Route path="/mindmap" element={<Mindmap />} />
+        <Route path="/questionaire" element={<Questionaire />} />
       </Routes>
     </BrowserRouter>
   );
